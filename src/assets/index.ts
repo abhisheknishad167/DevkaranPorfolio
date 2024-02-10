d5b31e2434495930cd364abce56f8fd151ae947d
Abhishek <Abhisheknishad167@gmail.com>
2024-02-10 18:12:52 +0530

    Porfolio

---
+++ src/assets/index.ts
@@ -0,0 +1,66 @@
+import logo from "./logo.png";
+import backend from "./java-icon.svg";
+import creator from "./creator.png";
+import mobile from "./react.svg";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import SpringBoot from "./tech/SpringBoot.svg";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import java from "./java-icon.svg";
+import devops from "./tech/AzureDevops.svg";
+
+import meta from "./company/meta.png";
+import Verzeo from "./company/verzeo.svg";
+import Quadwave from "./company/Quadwave.svg";
+import tesla from "./company/tesla.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  SpringBoot,
+  devops,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  java,
+  meta,
+  Verzeo,
+  Quadwave,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+};
